48c9477c4e30f5c2b9fb7fa6b70d43c108f08f7f
Nadia-Chen <67884201+Nadia-Chen@users.noreply.github.com>
2022-06-15 16:40:46 +0800

    DeepSilence-Web

---
+++ DeepSilence-Web/backend/args.py
@@ -0,0 +1,4 @@
+class Args(object):
+    def __init__(self):
+        self.model_path = "/path_to_the_model/librispeech_pretrained_v2.pth"
+        self.device = "cuda"
---
+++ DeepSilence-Web/backend/utils/secret.py
@@ -0,0 +1,6 @@
+class Secret(object):
+    def __init__(self):
+        self.account = "admin"
+        self.password = "set_admin_password"
+        self.level = "admin"
+        self.gift = "secret_token_for_invited_user"
